2422984193e18c8c2eda0fe3198a3d8c1c00644b
giannis349 <34054365+giannis349@users.noreply.github.com>
2023-12-30 13:15:54 +0200

    gg

--- aenaomp/src/pages/IndexPage.vue
+++ aenaomp/src/pages/IndexPage.vue
@@ -1,17 +1,48 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+   <div class="row full-width">
+    <div class="col-6 text-white">
+      <div class="q-ml-md q-mb-xl">{{ selected_movie.title }}</div>
+      <div class="q-ml-md">{{ selected_movie.description }}</div>
+    </div>
+    <div class="col-6">
+      <q-img
+        :src="selected_movie.poster"
+        :ratio="16/9"
+        spinner-color="primary"
+        spinner-size="82px"
+        class="q-ml-xl"
+        style="border-radius: 12px 0px 0px 12px; max-width: 95%;"
+      />
+    </div>
+   </div>
+   <div class="row full-width">
+    <q-img
+    class="q-ml-lg pointer"
+    v-for="movie in allmovies" :key="movie.id"
+      :src="movie.poster"
+      spinner-color="primary"
+      spinner-size="82px"
+      style="width: 250px; height: 350px; border-radius: 12px;"
+      @click="select_movie(movie)"
+      @mouseover="select_movie(movie)"
+      :style="selected_movie.title === movie.title ? 'box-shadow: 0px 0px 30px white': 'box-sandow: 0px 0px 0px white'"
+    />
+   </div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { ref, computed } from 'vue'
+import { useCounterStore } from '/src/stores/example-store'
+const store = useCounterStore();
 
-export default defineComponent({
-  name: 'IndexPage'
+const allmovies = computed(() => {
+  return store.allmovies
 })
+const selected_movie = ref(allmovies.value[0])
+
+const select_movie = (m) => {
+  selected_movie.value = m
+}
 </script>
--- aenaomp/src/stores/example-store.js
+++ aenaomp/src/stores/example-store.js
@@ -3,6 +3,20 @@ import { defineStore } from 'pinia';
 export const useCounterStore = defineStore('counter', {
   state: () => ({
     counter: 0,
+    allmovies: [
+      {
+        id: 1,
+        title: 'Indiana Jones and the Dial of Destiny',
+        description: 'Experience the return of legendary hero, Indiana Jones, in the fifth installment of this beloved swashbuckling series of films. Finding himself in a new era, approaching retirement, Indy wrestles with fitting into a world that seems to have outgrown him. But as the tentacles of an all-too-familiar evil return in the form of an old rival, Indy must don his hat and pick up his whip once more to make sure an ancient and powerful artifact doesn\'t fall into the wrong hands.',
+        poster: 'https://pbs.twimg.com/media/FxbxEb9aEAAqpOS?format=jpg&name=large'
+      },
+      {
+        id: 2,
+        title: ' Indiana Jones and the Last Crusade',
+        description: 'An art collector appeals to Indiana Jones to embark on a search for the Holy Grail. He learns that another archaeologist has disappeared while searching for the precious goblet, and the missing man is his own father, Dr. Henry Jones. The artifact is much harder to find than they expected, and its powers are too much for those impure of heart',
+        poster: 'https://i.ebayimg.com/images/g/aE8AAOSwrx1juLT-/s-l1600.jpg'
+      },
+    ]
   }),
   getters: {
     doubleCount: (state) => state.counter * 2,
